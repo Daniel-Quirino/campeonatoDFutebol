21cc07a79bfc0a737c607aa2ca83ec80d64fd9a1
Daniel Pires Quirino <danielpires6@hotmail.com>
2019-11-24 11:56:09 -0300

    Adicionado as tabelas clube e contrato

--- backup.sql
+++ backup.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `campeonatodefutebol` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `campeonatodefutebol`;
 -- MySQL dump 10.13  Distrib 8.0.13, for Win64 (x86_64)
 --
 -- Host: localhost    Database: campeonatodefutebol
@@ -16,6 +14,58 @@ USE `campeonatodefutebol`;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `clube`
+--
+
+DROP TABLE IF EXISTS `clube`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `clube` (
+  `CNPJ` float NOT NULL,
+  `estadioSede` varchar(30) DEFAULT NULL,
+  `nome` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`CNPJ`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `clube`
+--
+
+LOCK TABLES `clube` WRITE;
+/*!40000 ALTER TABLE `clube` DISABLE KEYS */;
+/*!40000 ALTER TABLE `clube` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `contrato`
+--
+
+DROP TABLE IF EXISTS `contrato`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `contrato` (
+  `codContrato` int(11) NOT NULL AUTO_INCREMENT,
+  `inicio` date DEFAULT NULL,
+  `fim` date DEFAULT NULL,
+  `CNPJ` float NOT NULL,
+  `codJogador` int(11) DEFAULT NULL,
+  PRIMARY KEY (`codContrato`),
+  KEY `fk_jogador_contrato` (`codJogador`),
+  KEY `fk_clube_contrato` (`CNPJ`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `contrato`
+--
+
+LOCK TABLES `contrato` WRITE;
+/*!40000 ALTER TABLE `contrato` DISABLE KEYS */;
+/*!40000 ALTER TABLE `contrato` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `gol`
@@ -42,6 +92,34 @@ LOCK TABLES `gol` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `jogador`
+--
+
+DROP TABLE IF EXISTS `jogador`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `jogador` (
+  `codJogador` int(11) NOT NULL AUTO_INCREMENT,
+  `posicao` varchar(30) DEFAULT NULL,
+  `primeiroNome` varchar(30) DEFAULT NULL,
+  `sobrenome` varchar(30) DEFAULT NULL,
+  `altura` float DEFAULT NULL,
+  `peso` float DEFAULT NULL,
+  `dataDeascimento` date DEFAULT NULL,
+  PRIMARY KEY (`codJogador`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `jogador`
+--
+
+LOCK TABLES `jogador` WRITE;
+/*!40000 ALTER TABLE `jogador` DISABLE KEYS */;
+/*!40000 ALTER TABLE `jogador` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Dumping routines for database 'campeonatodefutebol'
 --
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -54,4 +132,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-11-17 18:50:41
+-- Dump completed on 2019-11-24 11:54:39
